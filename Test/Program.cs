5d56b3cce91b5475f587fd8c43e3fe727c7268a7
Opilane TTHK <opilane@tthk.ee>
2021-11-23 16:33:04 +0200

    test

---
+++ Test/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace Ylesanne5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Random Facts so Interesting You'll Say, OMG! ");
+            string filePath = @"C:\Users\opilane\Samples\funfacts.txt";
+            string[] FunFacts = File.ReadAllLines(filePath);
+
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, FunFacts.Length);
+            Console.WriteLine(FunFacts[randomIndex]);
+
+            Console.WriteLine(GetRandomFact());
+        }
+
+        public static string GetRandomFact()
+        {
+            string filePath = @":\Users\opilane\Samples\funfacts.txt";
+            string[] FunFacts = File.ReadAllLines(filePath);
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, FunFacts.Length);
+
+            return FunFacts[randomIndex];
+        }
+    }
+}
